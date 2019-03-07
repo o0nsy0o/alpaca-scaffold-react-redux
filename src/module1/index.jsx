ffe1dcdf91b3bf84d3dc5ede49c627bed0b4792a
o0nsy0o <253431591@qq.com>
2019-03-07 14:56:50 +0800

    update

--- src/module1/index.jsx
+++ src/module1/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './app.css';
 
 ReactDom.render(
     <h1>hello, world!</h1>,
--- webpack/config/webpack.base.conf.js
+++ webpack/config/webpack.base.conf.js
@@ -1,30 +1,45 @@
-
-const paths = require('./paths');
-const PUBLIC_PATH = paths.appPublic;
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const paths = require('./paths');
 const chalk = require('chalk');
+const env = require('./env');
+const PUBLIC_PATH = paths.appPublic;
+const devMode = !env.isProdEnv();
 
 module.exports = {
   output: {
     path: PUBLIC_PATH,
     filename: '[name]/bundle[hash:8].js',
   },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx', '.json', '.less'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: { loader: "babel-loader" }
-      }
+        test: /\.(le|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader',
+        ],
+      },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: { loader: "babel-loader" } }
     ]
   },
   plugins: [
     new ProgressBarPlugin({
       format: chalk.cyan('build') + ' [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
       clear: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     })
   ]
 }
